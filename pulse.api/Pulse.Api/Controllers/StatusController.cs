05cf3a408f26b4910c5d238114153c08cbd39261
Ali <alistair.m.obrien@gmail.com>
2026-08-11 14:04:56 +0900

    test

--- pulse.api/Pulse.Api/Controllers/StatusController.cs
+++ pulse.api/Pulse.Api/Controllers/StatusController.cs
@@ -12,7 +12,7 @@ public class StatusController : ControllerBase
         return Ok(new
         {
             success = true,
-            msg = "all is good here",
+            msg = "all is good",
             version = Environment.GetEnvironmentVariable("PULSE_VERSION"),
             commit = Environment.GetEnvironmentVariable("PULSE_COMMIT_SHA"),
             date = Environment.GetEnvironmentVariable("PULSE_BUILD_DATE")
