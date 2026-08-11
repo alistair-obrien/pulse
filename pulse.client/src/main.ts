d9c58daa8c1956eedf62d73c04495c5e4315c1eb
Ali <alistair.m.obrien@gmail.com>
2026-08-11 18:15:07 +0900

    test

--- pulse.client/src/main.ts
+++ pulse.client/src/main.ts
@@ -66,7 +66,7 @@ const appConfig:AppConfig = {
 const googleClientId =
     import.meta.env.VITE_GOOGLE_LOGIN_WEBCLIENT_ID;
 
-console.log(
+console.error(
     "Google Web Client ID:",
     googleClientId
         ? googleClientId
